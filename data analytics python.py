b66048f5c42aa398577b7a3faa4ce195b7ec00af
Ankit2345chahar <ankitchahar272005@gmail.com>
2025-09-25 15:57:29 +0530

    commit7

--- data analytics python.py
+++ data analytics python.py
@@ -416,6 +416,28 @@
 
 # print(f"Largest number: {largest}")
 # print(f"Second largest number: {sec_largest}")
+   
+
+
+ #Check if List is sorted or not.
+
+# a = [12,13,14,15,16]
+# for i in range(len(a)-1):
+#     if a [i] < a [i+1]:
+#         continue
+#     else:
+#         print("your list is not sorted")
+#         break
+# else:
+#     print("your listed is sorted")
+
+# tuple data structure
+
+
+
+
+        
+
 
 
 
